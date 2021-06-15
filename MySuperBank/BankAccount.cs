7f9ba1cd40e99c6e6622d74a735918f26b4f93d2
Walter Ruganzu <rwalter692@gmail.com>
2021-06-15 10:38:39 +0300

    first commit

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        // private means it can only be accessed by thr code in this classs.
+        private static int accountNumberSeed = 1234567890;
+
+        public string Number { get; }
+        public string  Owner { get; set; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTansactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+
+        private List<Transaction> allTansactions = new List<Transaction>();
+
+
+        // This is a constuctor for the Bank
+        // This like a template of a bank account
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        // Displays the methods of the Bank Account.
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTansactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTansactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            //Header
+            report.AppendLine("date\t\tAmount\tNoate");
+            foreach (var item in allTansactions)
+            {
+                // Rows
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ MySuperBank/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MySuperBank
+{
+    public class Transaction
+    {
+        public decimal Amount { get;  }
+        public DateTime Date { get; }
+        public string Notes { get;  }
+
+        // Constuctor for the bank transactions.
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
